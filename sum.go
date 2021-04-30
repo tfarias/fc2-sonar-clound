e5307820e5c71e3cff4653c4cf8bb9a7ee464ba3
Tiago <tfariasg3@gmail.com>
2021-04-30 18:26:32 -0400

    ci: check is quality gate is work on sonar clound

--- sum.go
+++ sum.go
@@ -10,10 +10,10 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func subtracao(a int, b int) int {
-	return a - b
-}
+// func subtracao(a int, b int) int {
+// 	return a - b
+// }
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
